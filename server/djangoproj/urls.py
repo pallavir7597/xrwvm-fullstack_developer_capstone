e605e68967ae799a7a5d1ea27ae79ce92febb568
pallavir7597 <117059962+pallavir7597@users.noreply.github.com>
2023-11-29 21:40:15 -0800

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
     path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
 ]
